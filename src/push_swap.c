610982de712216c2e49824a49123984121b64d4e
gyellowj <avkim1225@gmail.com>
2020-09-11 12:25:29 +0300

    2 version

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,1 +1,28 @@
-þÿ
+//
+// Created by gyellowj on 09.09.2020.
+//
+
+#include "push_swap.h"
+
+
+#include <stdio.h>
+
+
+void 		push_swap(int ac, char **av)
+{
+	t_stack a;
+	av = ft_strsplit(*av, ' ');
+	while (*av)
+	{
+		a.head->number = ft_atoi(*av);
+		a.head = a.head->next;
+		*av++;
+	}
+	while (a.head->next)
+	{
+
+		printf("%i\n", a.head->number);
+
+		a.head = a.head->next;
+	}
+}
